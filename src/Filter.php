7558dc53d41aa434d0357c7c19db1d28b46fad33
Joe Grant <jmgrant1995@gmail.com>
2022-05-31 14:22:46 +0800

    initial commit

---
+++ src/Filter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Itsmejgrant\AjaxFilter;
+
+/**
+ * A tidy helper class for filtering content
+ */
+class Filter
+{
+    
+    public function __construct($name = '', $post_types = [], $labels = [], $args = [])
+    {
+        // Bind variables   
+        print_r('this runs');
+    }
+
+}
